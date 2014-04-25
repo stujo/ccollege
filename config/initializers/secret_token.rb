701690c075d2afaa9b2b64833fbd00163059ed7a
stujo <stujo@imagescript.com>
2014-04-25 13:36:21 -0700

    New with postgress, no testunit and rspec init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ccollege::Application.config.secret_key_base = '0ab66839d6d11e4147fd74dea2141bb8565a084b51a128837631d3dd9c413beea27ee0e46fbf48d21de268b1e9302bc89e5c3fbaaa3bd303434787fafca53b26'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ccollege::Application.config.session_store :cookie_store, key: '_ccollege_session'
